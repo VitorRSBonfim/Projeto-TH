2fb59a8cd8d8e19c7dba8d653f312bc139eeb584
VitorRSBonfim <vitorrsbonfim@gmail.com>
2023-01-26 16:29:07 -0300

    _

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var menu = document.querySelector('.menu-icon')
+var ul = document.querySelector('.ul')
+
+menu.addEventListener('click', ()=>{
+    if (ul.classList.contains('ativo')) {
+        ul.classList.remove('ativo')
+        document.querySelector('.menu-icon img').src ='imgs/menu.png';
+    }else{
+        ul.classList.add('ativo')
+        document.querySelector('.menu-icon img').src ='imgs/close.png'
+    }
+})
