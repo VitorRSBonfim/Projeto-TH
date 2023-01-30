02626375023a86173fce02f3261439bcde1f4901
Vitor Rodrigo <vitorrsbonfim@gmail.com>
2023-01-29 21:16:30 -0300

    _

--- js/equipe.js
+++ js/equipe.js
@@ -4,9 +4,9 @@ var ul = document.querySelector('.ul')
 menu.addEventListener('click', ()=>{
     if (ul.classList.contains('ativo')) {
         ul.classList.remove('ativo')
-        document.querySelector('.menu-icon img').src ='imgs/menu.png';
+        document.querySelector('.menu-icon img').src ='../imgs/menu.png';
     }else{
         ul.classList.add('ativo')
-        document.querySelector('.menu-icon img').src ='imgs/close.png'
+        document.querySelector('.menu-icon img').src ='../imgs/close.png'
     }
 })
